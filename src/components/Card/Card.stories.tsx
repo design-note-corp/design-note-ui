dd276897a3fdfa5dfdde7644d9975e49811faaea
knaot0 <naoto5018@gmail.com>
2022-05-02 23:49:54 +0900

    feat: Cardコンポーネントを追加

---
+++ src/components/Card/Card.stories.tsx
@@ -0,0 +1,29 @@
+import dayjs from "dayjs";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Card } from "./Card";
+
+export default {
+  title: "Card",
+  component: Card,
+} as ComponentMeta<typeof Card>;
+
+const Template: ComponentStory<typeof Card> = (args) => {
+  return <Card {...args} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  cover: (
+    <img src="https://placehold.jp/120x120.png" width="100%" height="100%" />
+  ),
+  title:
+    "長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル長いタイトル",
+  chips: ["Label A", "Label B", "Label C"],
+  avatar: (
+    <img src="https://placehold.jp/16x16.png" width="100%" height="100%" />
+  ),
+  nickname: "Taro Yamada",
+  createdAt: dayjs().subtract(2, "day").format(),
+  likeCount: 130,
+};
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,42 @@
+import { Gap, Space } from "../Space";
+import { CardContent } from "../CardContent";
+import { CardCover } from "../CardCover";
+import { CardTitle } from "../CardTitle";
+import { CardChips } from "../CardChips";
+import { CardDescription } from "../CardDescription";
+
+type CardProps = {
+  cover: React.ReactNode;
+  title: string;
+  chips?: string[];
+  avatar: React.ReactNode;
+  nickname: string;
+  createdAt: string;
+  likeCount: number;
+};
+
+export const Card: React.FC<CardProps> = ({
+  cover,
+  title,
+  chips = [],
+  avatar,
+  nickname,
+  createdAt,
+  likeCount,
+}) => {
+  return (
+    <Space columnGap={Gap.Large}>
+      <CardCover>{cover}</CardCover>
+      <CardContent>
+        <CardTitle>{title}</CardTitle>
+        <CardChips labels={chips} />
+        <CardDescription
+          avatar={avatar}
+          nickname={nickname}
+          createdAt={createdAt}
+          likeCount={likeCount}
+        />
+      </CardContent>
+    </Space>
+  );
+};
