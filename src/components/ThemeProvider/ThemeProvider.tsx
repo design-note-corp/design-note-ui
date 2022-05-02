07c63bc1a24250f2511343a8ee08d1f94ac1d5a5
knaot0 <naoto5018@gmail.com>
2022-05-02 13:12:41 +0900

    feat: ThemeProviderのpropsからclassNameを渡せるようにする

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -1,7 +1,12 @@
-import { themeClass } from "../../styles";
+import { themeClass as defaultThemeClass } from "../../styles";
 
-type ThemeProviderProps = React.PropsWithChildren<{}>;
+type ThemeProviderProps = React.PropsWithChildren<{
+  themeClass?: string;
+}>;
 
-export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({
+  children,
+  themeClass = defaultThemeClass,
+}) => {
   return <div className={themeClass}>{children}</div>;
 };
