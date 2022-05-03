c2a635ba1870ba300cb157c3cf1ba010ad55a9c9
knaot0 <naoto5018@gmail.com>
2022-05-03 21:03:30 +0900

    feat: add github icon

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -2,6 +2,7 @@ export { default as Add } from "./Add";
 export { default as Bell } from "./Bell";
 export { default as Edit } from "./Edit";
 export { default as Favorite } from "./Favorite";
+export { default as Github } from "./Github";
 export { default as Instagram } from "./Instagram";
 export { default as Search } from "./Search";
 export { default as Twitter } from "./Twitter";
