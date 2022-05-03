f871d429182a5a4968387f59a9d504a7e81eade1
Naoto <40537111+knaot0@users.noreply.github.com>
2022-05-03 12:25:39 +0900

    feat: font-familyを設定 (#51)

---
+++ src/styles/app.css.ts
@@ -0,0 +1,6 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+globalStyle("body", {
+  fontFamily:
+    'Inter ,-apple-system,"BlinkMacSystemFont","Hiragino Kaku Gothic ProN","Hiragino Sans",Meiryo,sans-serif',
+});
