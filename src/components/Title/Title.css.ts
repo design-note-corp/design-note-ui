8e5db63de3851dc64e46db1969fdf35a012bca16
knaot0 <naoto5018@gmail.com>
2022-05-03 17:58:40 +0900

    chore: Titleコンポーネントのmarginを0にする

--- src/components/Title/Title.css.ts
+++ src/components/Title/Title.css.ts
@@ -1,3 +1,21 @@
 import { style } from "@vanilla-extract/css";
 
-export const className = style({});
+export const h1 = style({
+  margin: 0,
+});
+
+export const h2 = style({
+  margin: 0,
+});
+
+export const h3 = style({
+  margin: 0,
+});
+
+export const h4 = style({
+  margin: 0,
+});
+
+export const h5 = style({
+  margin: 0,
+});
