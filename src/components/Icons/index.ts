718cd5b64949a9a2c1123b14096051decd5cce6c
knaot0 <naoto5018@gmail.com>
2022-05-03 21:05:11 +0900

    feat: add internet icon

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -4,6 +4,7 @@ export { default as Edit } from "./Edit";
 export { default as Favorite } from "./Favorite";
 export { default as Github } from "./Github";
 export { default as Instagram } from "./Instagram";
+export { default as Internet } from "./Internet";
 export { default as Link } from "./Link";
 export { default as Search } from "./Search";
 export { default as Twitter } from "./Twitter";
