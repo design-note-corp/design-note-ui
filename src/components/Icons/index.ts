37dfc81e1007bc0bbe1933be494058df7d416e80
knaot0 <naoto5018@gmail.com>
2022-05-03 21:02:40 +0900

    feat: add instagram icon

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -2,5 +2,6 @@ export { default as Add } from "./Add";
 export { default as Bell } from "./Bell";
 export { default as Edit } from "./Edit";
 export { default as Favorite } from "./Favorite";
+export { default as Instagram } from "./Instagram";
 export { default as Search } from "./Search";
 export { default as Twitter } from "./Twitter";
